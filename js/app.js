cbd2671f6fc0d873650ac0d3f749ddd0c3dfd5b6
Anvay Bhanap <anvaybhanap@Anvays-MacBook-Pro.local>
2021-06-08 17:32:43 -0700

    Completed JS code

--- js/app.js
+++ js/app.js
@@ -45,8 +45,8 @@ if (answer4 === "yes" || answer4 === "y"){
   alert("Yea, I still live with my parents and sister. The only possible way that I'll move out is if I get a job in SF or somewhere out of Silicon Valley. I'm not too keen on moving out of Silicon Valley but other than that, fingers crossed 🤞");
 } else {alert("Make sure to enter a Yes or No answer; I can't understand if you type in anything else.")}
 
-let question5 = prompt("Do I like coding");
-let answer5 = question3.toLowerCase();
+let question5 = prompt("Do I like coding?");
+let answer5 = question5.toLowerCase();
 if (answer5 === "yes" || answer5 === "y"){
   //console.log("I think I have a similar sentiment to most coders, in that I like it until I come across a seemingly insurmountable bug, which ends up being a missing ; on line 73. \nSo in conclusion: mostly yes.");
   alert("I think I have a similar sentiment to most coders, in that I like it until I come across a seemingly insurmountable bug, which ends up being a missing ; on line 73. \nSo in conclusion: mostly yes.");
@@ -54,5 +54,8 @@ if (answer5 === "yes" || answer5 === "y"){
   //console.log("I wouldn't really be in my major or even this course if I didn't like it.");
   alert("I wouldn't really be in my major or even this course if I didn't like it.");
 } else {alert("Make sure to enter a Yes or No answer; I can't understand if you type in anything else.")}
+
+//console.log("Thank you for playing my game, " + userName + ". You can learn more about me after you press 'OK'.");
+alert("Thank you for playing my game, " + userName + ". You can learn more about me after you press 'OK'.");
 
 
